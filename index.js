2f194634bad39db2eee790d11ccf073dcad3d595
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-26 18:52:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const sharp = require('sharp')
+const sharp = require('sharp');
 
 const strategy = {
   balanced: 0,
@@ -16,14 +16,14 @@ const correction = {
 
 class rMQR {
   async generate(text, options={}) {
-    if (typeof text !== 'String') {
-      throw new Error('Expected String recived instance of '+typeof text)
+    if ((typeof text).toLowerCase() !== 'string') {
+      throw new Error('Expected String recived instance of '+typeof text);
     }
-    if (!text) {
-      throw new Error('Input must be atleast one letter')
+    if (text.length < 1) {
+      throw new Error('Input must be atleast one letter');
     }
     if (text.length > (150 - (options?.correction === correction.high ? 76 : 0))) {
-      throw new Error('Input must be atleast one letter')
+      throw new Error('Input is too long');
     }
     if (options.strategy) {
       if (!Object.values(strategy).includes(options.strategy)) {
@@ -44,10 +44,10 @@ class rMQR {
       },
       body: JSON.stringify({
         text: text,
-        versionStrategy: ['balanced','minimize_height','minimize_width'][options?.strategy || 0],
-        errorCorrectionLevel: ['auto','m','h'][options?.correction || 0]
+        versionStrategy: ['balanced','minimize_height','minimize_width'][options?.strategy ?? 0],
+        errorCorrectionLevel: ['auto','m','h'][options?.correction ?? 0]
       })
-    })
+    });
     let res = await req.json();
     return res;
   }
@@ -56,30 +56,38 @@ class rMQR {
       jpg: 'jpeg'
     }
     if (Object.keys(alias).includes(format)) format = alias[format];
-    let qr = new sharp(Buffer.from(data.qr.flat().map(p=>!p*255)), {
-      raw: {
-        width: data.width,
-        height: data.height,
-        channels: 1
-      }
-    })
-      .toFormat(format)
-      .resize(data.width*size, data.height*size, {
-        kernel: sharp.kernel.nearest
-      })
-      .toBuffer()
-      .then(outputBuffer => {
-        Object.keys(alias).map(key => {alias[alias[key]] = key; delete alias[key]});
-        if (Object.keys(alias).includes(format)) format = alias[format];
-        switch (type) {
-          case 'buffer':
-            return outputBuffer;
-          case 'uri':
-            return 'data:image/'+format+';base64,'+outputBuffer.toString('base64');
-          default:
-            throw new Error('Unsoported type')
+    return new Promise((resolve, reject) => {
+      let qr = new sharp(Buffer.from(data.qr.flat().map(p=>!p*255)), {
+        raw: {
+          width: data.width,
+          height: data.height,
+          channels: 1
         }
       })
+        .toFormat(format)
+        .resize(data.width*size, data.height*size, {
+          kernel: sharp.kernel.nearest
+        })
+        .toBuffer()
+        .then(outputBuffer => {
+          Object.keys(alias).map(key => {alias[alias[key]] = key; delete alias[key]});
+          if (Object.keys(alias).includes(format)) format = alias[format];
+          switch (type) {
+            case 'buffer':
+              resolve(outputBuffer);
+              break;
+            case 'uri':
+              resolve(`data:image/${format};base64,${outputBuffer.toString('base64')}`);
+              break;
+            default:
+              reject('Unsoported type');
+              break;
+          }
+        })
+        .catch(err=>{
+          reject(err);
+        })
+    });
   }
 }
 
